66df0e67543d2418344e049cfb60cd07f3affe89
Bilikis Orulebaja <bilkisbalogun@gmail.com>
2018-10-02 04:40:48 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -14,13 +14,13 @@ def key_for_min_value(name_hash)
 end
 
 #def key_for_min_value(hash) 
-  smallest_key = nil
-  minimal_value = nil
-  hash.each do |key, value|
-    if tiniest_value == nil || value < tiniest_value
-      tiniest_value = value
-      smallest_key = key
-    end
-  end
-  smallest_key
-end
+ # smallest_key = nil
+ # minimal_value = nil
+ # hash.each do |key, value|
+ #   if tiniest_value == nil || value < tiniest_value
+ #     tiniest_value = value
+ #     smallest_key = key
+ #   end
+#  end
+#  smallest_key
+#end
